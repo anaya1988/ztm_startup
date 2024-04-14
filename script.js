26ebffa3e540c7a697119049ecd12e24169a7278
Marc <166598413+anaya1988@users.noreply.github.com>
2024-04-14 09:19:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = prompt("Wie heißt Du?");
+if (name === "Romana") {
+    alert("Hallo kleiner Hüdi!");
+} else if (name === "Marc") {
+    alert("Hallo großer Hüdi");
+} else if (name === "Flo") {
+    alert("Arschloch!");
+} else if (name === "Wauwitaubi") {
+    alert("Wauwi wi taubibi!");
+} else if (name === "Taubi") {
+    alert("Wauwi Taubi!");
+} else if (name === "Wauwi") {
+    alert("Wauwi Taubi!");
+} else if (name === "Andi") {
+    alert("Hallo Houdini");
+} else alert("Hallo " + name);  
