d5537da44d904d4dece22ba2751fbd5933517b1d
Marc <166598413+anaya1988@users.noreply.github.com>
2024-04-21 09:40:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,30 @@ whichListItem.addEventListener("click", toggleDoneOrRemove)
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keydown", addListAfterKeypress);
 isDeleteAll.addEventListener("click", clearUl)
+
+// +++ BACKGROUND GENERATOR +++ \\
+
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("gradient")
+
+color1.addEventListener("input", function() {
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+    }
+)
+
+
+color2.addEventListener("input", function() {
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+    }
+)
